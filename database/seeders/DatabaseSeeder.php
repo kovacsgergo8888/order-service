bd25a069aeb695fb92344af7978a8671328baa87
Gergely Kovács <kovacs.gergely8888@gmail.com>
2023-10-19 20:37:52 +0200

    seed some order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Order;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,35 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        for ($i = 0; $i < 1000; $i++) {
+            $order = Order::create([
+                'name' => 'Johnny Silverhand',
+                'email'=> 'arasaki@hater.com',
+                'shipping_type' => 'COURIER_SHIPPING',
+                'shipping_name' => 'Shipping John',
+                'shipping_city' => 'Night City',
+                'shipping_postcode' => '1234',
+                'shipping_address' => 'Street 1',
+                'billing_name' => 'Shipping John',
+                'billing_city' => 'Night City',
+                'billing_postcode' => '1234',
+                'billing_address' => 'Street 1',
+            ]);
+
+            $order->orderProducts()->createMany([
+                [
+                    'name' => 'Gun',
+                    'price' => 1000,
+                    'quantity' => rand(1, 5)
+                ],
+                [
+                    'name' => 'Medpack',
+                    'price' => 100,
+                    'quantity' => rand(1, 5)
+                ]
+            ]);
+        }
+
     }
 }
