431c29761ee0eb6fc638972c70bfd1680d435c8e
Gergely Kovács <kovacs.gergely8888@gmail.com>
2023-10-20 08:57:48 +0200

    swagger docs

--- app/Http/Controllers/CreateOrderController.php
+++ app/Http/Controllers/CreateOrderController.php
@@ -16,18 +16,18 @@ class CreateOrderController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'status' => OrderStatus::NEW->value,
-            'shipping_type' => ShippingType::from($data['shippingType'])->value,
-            'shipping_name' => $data['shippingName'],
-            'shipping_postcode' => $data['shippingPostcode'],
-            'shipping_city' => $data['shippingCity'],
-            'shipping_address' => $data['shippingAddress'],
-            'billing_name' => $data['billingName'],
-            'billing_postcode' => $data['billingPostcode'],
-            'billing_city' => $data['billingCity'],
-            'billing_address' => $data['billingAddress'],
+            'shipping_type' => ShippingType::from($data['shipping_type'])->value,
+            'shipping_name' => $data['shipping_name'],
+            'shipping_postcode' => $data['shipping_postcode'],
+            'shipping_city' => $data['shipping_city'],
+            'shipping_address' => $data['shipping_address'],
+            'billing_name' => $data['billing_name'],
+            'billing_postcode' => $data['billing_postcode'],
+            'billing_city' => $data['billing_city'],
+            'billing_address' => $data['billing_address'],
         ]);
 
-        $order->orderProducts()->createMany($data['orderProducts']);
+        $order->orderProducts()->createMany($data['order_products']);
 
         return response()->json(['order_id' => $order->order_id]);
     }
